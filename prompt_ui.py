1118636bfef6eae8ac4e215c253a952a0695594e
anish1A1 <alloozpat87@gmail.com>
2025-11-14 08:28:45 +0545

    used hugging face api to communicate with the chat model. Used streamlit for UI

--- prompt_ui.py
+++ prompt_ui.py
@@ -1,6 +1,7 @@
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 from dotenv import load_dotenv
 
+
 import streamlit as st 
 load_dotenv()
 
@@ -13,3 +14,11 @@ llm = HuggingFaceEndpoint(
 model = ChatHuggingFace(llm=llm)
 
 st.header('Research Tool')
+
+user_input = st.text_input("Enter your prompt: ")
+
+if st.button('Summarize'):
+    result = model.invoke(user_input)
+    # The actual content from chatmodel to display
+    st.write(result.content)
+    print(result.content)
