f3690e3d010038dc14c3654888afeeeb69e7dc68
anish1A1 <alloozpat87@gmail.com>
2025-11-15 09:02:13 +0545

    used the invoke method only one time

--- dynamic_prompt_ui.py
+++ dynamic_prompt_ui.py
@@ -26,6 +26,10 @@ length_input = st.selectbox( "Select Explanation Length", ["Short (1-2 paragraph
 template = load_prompt('template.json')
 
 
+# Wrote this way to prevent streamlit for reading this
+_ = """
+# We can use this easy way where we need to use the invoke function 2 times
+
 # fills the placeholders 
 prompt = template.invoke({
     'paper_input': paper_input,
@@ -37,3 +41,21 @@ if st.button("Summarize"):
     result = model.invoke(prompt)
     print(result.content)
     st.write(result.content)
+    
+
+"""
+
+
+    
+# We can also create a chain where the template and model can be invoked together which is generally preferred in Langchain ecosystem.
+# With this we only need to use the invoke function one time.
+if st.button("Summarize"):
+    chain = template | model
+    result = chain.invoke({
+        'paper_input': paper_input,
+        'style_input': style_input,
+        'length_input': length_input
+    })
+    print(result.content)
+    st.write(result.content)
+    
