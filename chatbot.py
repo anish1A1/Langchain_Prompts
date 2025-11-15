b9a2df150ea45e14266b412bcae4699ad8250082
anish1A1 <alloozpat87@gmail.com>
2025-11-15 21:00:59 +0545

    clearly mentioned types of user in the chatbot

--- chatbot.py
+++ chatbot.py
@@ -1,5 +1,6 @@
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 from dotenv import load_dotenv
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 
 load_dotenv()
 
@@ -25,18 +26,20 @@ while True:
 # Now to get the chatbot to know previous chat history with the present
 # We can create a list which stores messages (both AI and human).
 
-chat_history = []
+chat_history = [
+    SystemMessage(content='You are a helpful AI Assistant')
+]
 
 while True:
     user_input = input("You: ")
-    chat_history.append(user_input)
+    chat_history.append(HumanMessage(content=user_input))
     
     if user_input == 'exit':
         break
     result = model.invoke(chat_history)
     # Added chat_history as it contains present as well as past messages.
     
-    chat_history.append(result.content) #storing the present AI message in history
+    chat_history.append(AIMessage(content=result.content)) #storing the present AI message in history
     print("AI: ", result.content)
     
 print(chat_history) 
